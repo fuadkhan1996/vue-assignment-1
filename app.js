c53c89ed0f35fe3a7b86135964053b8c9dfda8d8
fuadkhan1996 <fuad.khan@thedevden.co>
2021-01-16 19:15:32 +0500

    Display details

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      name: 'Fuad Khan',
+      age: 23,
+      imageURL: 'https://media.istockphoto.com/photos/pakistan-monument-islamabad-picture-id535695503?k=6&m=535695503&s=612x612&w=0&h=uP8aDK4xlfjk3kEiyr9wwUiuh80UwAiICweFpiBDosk=',
+    }
+  },
+  methods: {
+    getRandomNumber() {
+      return Math.random();
+    }
+  }
+})
+
+app.mount('#assignment')
